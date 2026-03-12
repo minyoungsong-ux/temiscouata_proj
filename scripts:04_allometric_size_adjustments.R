f20bd245149a0889247ed72b5ae6368aa22efec8
Sarah Minyoung Song <sarahsong854@gmail.com>
2026-03-12 16:06:20 -0400

    Update scripts:04_allometric_size_adjustments.R

--- scripts:04_allometric_size_adjustments.R
+++ scripts:04_allometric_size_adjustments.R
@@ -5,18 +5,35 @@
 # ============================================
 library(tidyverse)
 
-data.log <- stickleback_all |>
+# ALTERNATIVE CODE TO INCLUDE CMN STICKLEBACK WITH N/As, STILL MISSING SOME
+# CONTEMPORARY FISH WITH HALF BODIES
+# data.log <- stickleback_analysis |>
+#  filter(sl > 0) |>
+#  mutate(across(c(sl, body_depth, ap_length, ap_width, jaw_length,
+#                  ppl, ppw, dorsal_1, dorsal_2, pspine_r, pspine_l),
+#                ~ log10(.)))
+
+data.log <- stickleback_analysis |>
+  filter(across(c(sl, body_depth, ap_length, ap_width, jaw_length,
+                  ppl, ppw, dorsal_1, dorsal_2, pspine_r, pspine_l),
+                ~ . > 0)) |>
   mutate_at(vars(sl, body_depth, ap_length, ap_width, jaw_length, ppl, ppw, dorsal_1,
                  dorsal_2, pspine_r, pspine_l),
             log10)
 
 # size.coef is a function to get coefficient for #size adjustment 
 # trait - name of trait col to get #slope coefficient
+# altered code to account for NA and INF values
 size.coef<-function(traits){
-  ancova=lm(data=data.log, traits~sl+lake)
-  coef=ancova$coefficients[2]
+  ancova <- lm(traits ~ sl + lake, data=data.log, na.action = na.omit)
+  coef <- ancova$coefficients[2]
   coef
 }
+
+data.log <- data.log |>
+  mutate(across(c(sl, body_depth, ap_length, ap_width, jaw_length,
+                  ppl, ppw, dorsal_1, dorsal_2, pspine_r, pspine_l),
+                ~ ifelse(is.finite(.), ., NA)))
 
 # The following line produces slope coefficient. Repeat for each trait.
 BD.coef<-size.coef(data.log$body_depth)
@@ -27,17 +44,6 @@ PPL.coef<-size.coef(data.log$ppl)
 PPW.coef<-size.coef(data.log$ppw)
 D1.coef<-size.coef(data.log$dorsal_1)
 D2.coef<-size.coef(data.log$dorsal_2)
-
-data.log <- stickleback_all |>
-  filter(pspine_r > 0, pspine_l > 0) |>
-  mutate(
-    across(
-      c(sl, body_depth, ap_length, ap_width, jaw_length,
-        ppl, ppw, dorsal_1, dorsal_2, pspine_r, pspine_l),
-      log10
-    )
-  )
-
 PSR.coef<-size.coef(data.log$pspine_r)
 PSL.coef<-size.coef(data.log$pspine_l)
 
@@ -45,13 +51,42 @@ PSL.coef<-size.coef(data.log$pspine_l)
 Coef<-matrix(c(BD.coef, APL.coef, APW.coef,JL.coef, PPL.coef, PPW.coef, D1.coef, D2.coef, PSR.coef, PSL.coef),dimnames = list(c("body_depth", "ap_length", "ap_width", "jaw_length", "ppl", "ppw", "dorsal_1", "dorsal_2", "pspine_r", "pspine_l")))
 
 # Adjusts trait value using formula: Xadj=Xi*(mean(SL)/SLi)^b
-stickleback_all$body_depth.adj<-stickleback_all$body_depth*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["body_depth",]
-stickleback_all$ap_length.adj<-stickleback_all$ap_length*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["ap_length",]
-stickleback_all$ap_width.adj<-stickleback_all$ap_width*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["ap_width",]
-stickleback_all$jaw_length.adj<-stickleback_all$jaw_length*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["jaw_length",]
-stickleback_all$ppl.adj<-stickleback_all$ppl*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["ppl",]
-stickleback_all$ppw.adj<-stickleback_all$ppw*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["ppw",]
-stickleback_all$dorsal_1.adj<-stickleback_all$dorsal_1*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["dorsal_1",]
-stickleback_all$dorsal_2.adj<-stickleback_all$dorsal_2*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["dorsal_2",]
-stickleback_all$pspine_r.adj<-stickleback_all$pspine_r*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["pspine_r",]
-stickleback_all$pspine_l.adj<-stickleback_all$pspine_l*(mean(stickleback_all$sl)/stickleback_all$sl)^Coef["pspine_l",]
+stickleback_analysis$body_depth.adj<-stickleback_analysis$body_depth*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["body_depth",]
+stickleback_analysis$ap_length.adj<-stickleback_analysis$ap_length*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["ap_length",]
+stickleback_analysis$ap_width.adj<-stickleback_analysis$ap_width*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["ap_width",]
+stickleback_analysis$jaw_length.adj<-stickleback_analysis$jaw_length*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["jaw_length",]
+stickleback_analysis$ppl.adj<-stickleback_analysis$ppl*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["ppl",]
+stickleback_analysis$ppw.adj<-stickleback_analysis$ppw*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["ppw",]
+stickleback_analysis$dorsal_1.adj<-stickleback_analysis$dorsal_1*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["dorsal_1",]
+stickleback_analysis$dorsal_2.adj<-stickleback_analysis$dorsal_2*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["dorsal_2",]
+stickleback_analysis$pspine_r.adj<-stickleback_analysis$pspine_r*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["pspine_r",]
+stickleback_analysis$pspine_l.adj<-stickleback_analysis$pspine_l*(mean(stickleback_analysis$sl)/stickleback_analysis$sl)^Coef["pspine_l",]
+
+# Allometric size adjustments for gill rakers?
+raker.log <- gill_spatial |>
+  mutate_at(vars(SL, mean_length_mm, mean_width_mm),
+            log10)
+
+size.coef<-function(traits){
+  ancova=lm(data=raker.log, traits~SL+lake)
+  coef=ancova$coefficients[2]
+  coef
+}
+
+rakerlength.coef<-size.coef(raker.log$mean_length_mm)
+rakerwidth.coef<-size.coef(raker.log$mean_width_mm)
+
+Coef<-matrix(c(rakerlength.coef, rakerwidth.coef),dimnames = list(c("mean_length_mm", "mean_width_mm")))
+
+gill_spatial$mean_length_mm.adj<-gill_spatial$mean_length_mm*(mean(gill_spatial$SL)/gill_spatial$SL)^Coef["mean_length_mm",]
+gill_spatial$mean_width_mm.adj<-gill_spatial$mean_width_mm*(mean(gill_spatial$SL)/gill_spatial$SL)^Coef["mean_width_mm",]
+
+# PLOTS AFTER ALLOMETRIC SIZE ADJUSTMENTS:
+# PCA? (unfinished)
+pca_data <- data.log[, c("lake","year", traits)]
+
+# remove NA
+pca_data <- pca_data[complete.cases(pca_data), ]
+
+# remove Inf
+pca_data <- pca_data[!apply(pca_data[,traits],1,function(x) any(is.infinite(x))), ]
